bf350204fe19f717f2771ade8de1cfd676776987
sherlock457 <archan123mandal@gmail.com>
2024-08-11 19:19:05 +0530

    fixed

--- frontend/src/components/Quote.tsx
+++ frontend/src/components/Quote.tsx
@@ -3,13 +3,7 @@ export const Quote = () => {
         <div className="flex justify-center">
             <div className="max-w-lg">
                 <div className="text-3xl font-bold">
-                    "The customer support I received was exceptional. The support team went above and beyond to address my concerns"
-                </div>
-                <div className="max-w-md text-xl font-semibold text-left mt-4">
-                    Julies Winfield
-                </div>
-                <div className="max-w-md text-sm font-light text-slate-400">
-                    CEO | Acme corp
+                Welcome! This is a community space where everyone can share, read, and connect. Post your blogs, join the conversation, and let's grow together!
                 </div>
             </div>
         </div>
